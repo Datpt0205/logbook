ae5c5c9f72302d78096733056c3543a20c473737
Datpt0205 <86192047+Datpt0205@users.noreply.github.com>
2022-10-20 02:35:37 +0700

    Add Url and display image

--- app/src/main/java/com/example/exercise2/MainActivity.java
+++ app/src/main/java/com/example/exercise2/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.exercise2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
-    private String[] imageList;
-    private int index = 0;
+    ImageView imageView;
+    ArrayList<String> list = new ArrayList<String>();
+    int index = 0;
+    Button addUrl;
+    EditText url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +28,41 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.image_view);
 
-        imageList = getResources().getStringArray(R.array.images);
+        url = findViewById(R.id.url);
 
-        String url = "https://i.imgur.com/Gg6BpGn.jpeg";
-
-        loadImage();
+        addUrl = findViewById(R.id.addUrl);
+        addUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url_image = url.getText().toString().trim();
+                if(url_image.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Required Url", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    list.add(url_image);
+                    Toast.makeText(MainActivity.this, "Add url image successfully", Toast.LENGTH_SHORT).show();
+                    loadImage();
+                    url.setText("");
+                }
+            }
+        });
+//        list.add(0, "https://i.imgur.com/Q9WPlWA.jpeg");
+//        list.add(1, "https://i.imgur.com/oPj4A8u.jpeg");
+//        list.add(2, "https://i.imgur.com/MWAcQRM.jpeg");
+//        list.add(3, "https://i.imgur.com/Lnt9K7l.jpeg");
+//        list.add(4, "https://i.imgur.com/Gg6BpGn.jpeg");
     }
 
     public void loadImage(){
         Glide.with(MainActivity.this)
-                .load(imageList[index])
+                .load(list.get(index))
                 .centerCrop()
                 .into(imageView);
     }
 
     public void nextImage(View view){
         index++;
-        if(index >= imageList.length)
+        if(index >= list.size())
             index = 0;
         loadImage();
     }
@@ -45,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
     public void previousImage(View view){
         index--;
         if(index <= -1)
-            index = imageList.length -1;
+            index = list.size() -1;
         loadImage();
     }
 }
