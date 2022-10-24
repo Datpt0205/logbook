2c8af63155f1efc381f8a99025b7c3c6d8cd0d7c
Datpt0205 <86192047+Datpt0205@users.noreply.github.com>
2022-10-24 16:08:40 +0700

    Fix next and previous button

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private ArrayList<String> list;
     private int index = 0;
+    private Button btn_next, btn_previous;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,33 +26,87 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.image_view);
 
         list = new ArrayList<String>();
-        list.add(0, "https://i.imgur.com/Q9WPlWA.jpeg");
-        list.add(1, "https://i.imgur.com/oPj4A8u.jpeg");
-        list.add(2, "https://i.imgur.com/MWAcQRM.jpeg");
-        list.add(3, "https://i.imgur.com/Lnt9K7l.jpeg");
-        list.add(4, "https://i.imgur.com/Gg6BpGn.jpeg");
+        list.add( "https://i.imgur.com/Q9WPlWA.jpeg");
+        list.add( "https://i.imgur.com/oPj4A8u.jpeg");
+        list.add( "https://i.imgur.com/MWAcQRM.jpeg");
+        list.add( "https://i.imgur.com/Lnt9K7l.jpeg");
+        list.add( "https://i.imgur.com/Gg6BpGn.jpeg");
 
-        loadImage();
-    }
-
-    public void loadImage(){
         Glide.with(MainActivity.this)
                 .load(list.get(index))
                 .centerCrop()
                 .into(imageView);
+
+        btn_next = findViewById(R.id.btn_next);
+        btn_previous = findViewById(R.id.btn_previous);
+
+        if(index == 0)
+        {
+            btn_previous.setEnabled(false);
+        }
+
+        btn_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index --;
+                Glide.with(MainActivity.this)
+                        .load(list.get(index))
+                        .centerCrop()
+                        .into(imageView);
+                if(index == 0){
+                    btn_previous.setEnabled(false);
+                }
+                if(index > 0){
+                    btn_previous.setEnabled(true);
+                }
+                if(index == Integer.valueOf(list.size()-1))
+                {
+                    btn_next.setEnabled(false);
+                }else
+                {
+                    btn_next.setEnabled(true);
+                }
+            }
+        });
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index ++;
+                Glide.with(MainActivity.this)
+                        .load(list.get(index))
+                        .centerCrop()
+                        .into(imageView);
+                if(index == 0){
+                    btn_previous.setEnabled(false);
+                }
+                if(index > 0){
+                    btn_previous.setEnabled(true);
+                }
+                if(index == Integer.valueOf(list.size()-1))
+                {
+                    btn_next.setEnabled(false);
+                }else
+                {
+                    btn_next.setEnabled(true);
+                }
+
+            }
+        });
+
     }
 
-    public void nextImage(View view){
-        index++;
-        if(index >= list.size())
-            index = 0;
-        loadImage();
-    }
-
-    public void previousImage(View view){
-        index--;
-        if(index <= -1)
-            index = list.size() -1;
-        loadImage();
-    }
+//    public void nextImage(View view){
+//        index++;
+//        if(index >= list.size())
+//            index = 0;
+//        loadImage();
+//    }
+//
+//    public void previousImage(View view){
+//        index--;
+//        if(index <= -1)
+//            index = list.size() -1;
+//        loadImage();
+//    }
 }
